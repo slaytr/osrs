4a8cfa980f6dd854d6262d801ce492513a9eedd2
Bill Li <bill.li.dev@gmail.com>
2022-12-25 21:11:29 +1300

    switch backend to graphql

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,41 @@
-import express from 'express';
-import { Request, Response } from 'express';
+// import express from 'express';
+// import { Request, Response } from 'express';
+//
+// const app = express();
+//
+// app.get('/', (req: Request, res: Response) => {
+//     res.send('Application works!');
+// });
+//
+// app.listen(3000, () => {
+//     console.log('Application restarted');
+// });
+
+import express from 'express'
+import { graphqlHTTP } from 'express-graphql'
+import { buildSchema } from 'graphql'
+
+// Construct a schema, using GraphQL schema language
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+// The root provides a resolver function for each API endpoint
+const root = {
+    hello: () => {
+        return 'Hello world!';
+    },
+};
 
 const app = express();
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+}));
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Application works!');
-});
-
-app.listen(3000, () => {
-    console.log('Application started on port 3000!');
-});
+app.listen(4000, () => {
+    console.log('Running a GraphQL API server at http://localhost:4000/graphql');
+})

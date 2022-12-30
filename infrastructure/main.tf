1e3051078122ba1ac7e88661782101a3ac3794f3
Bill Li <bill.li.dev@gmail.com>
2022-12-30 21:18:33 +1300

    fix lambda zip, add dev compile command

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -10,6 +10,6 @@ provider "aws" {
     iam      = "http://localhost:4566"
     lambda   = "http://localhost:4566"
     dynamodb = "http://localhost:4566"
-    s3       = "http://localhost:4566"
+    s3       = "http://s3.localhost.localstack.cloud:4566"
   }
 }
